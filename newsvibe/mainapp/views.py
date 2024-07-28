8d075891193d336f4bb4bb1f9bc086a5b1914f03
ashish-nagmoti <ashishnagmoti5@gmail.com>
2024-07-28 12:55:11 +0530

    Refactor pagination logic in views.py and update home.html template

--- newsvibe/mainapp/views.py
+++ newsvibe/mainapp/views.py
@@ -3,17 +3,26 @@ from django.http import HttpResponse
 from .models import article
 from django.shortcuts import get_object_or_404
 from .scrapper import scrape_main
-import random
+from django.core.paginator import Paginator
 def home(request):
     articles = article.objects.all()
-    return render(request, "home.html",{'n':articles})
+    paginator = Paginator(articles,10)
+    news_page = request.GET.get('page',1)
+    page_obj = paginator.get_page(news_page)
+    try :
+        page_obj = paginator.page(news_page)
+    except Exception as E:
+        news_page =1
+        page_obj = paginator.page(news_page)
+    return render(request, "home.html",{'n':page_obj})
 
 def load_news(request):
     news_url =  "https://www.thehindu.com/news/"
     articles = scrape_main(news_url)
     news2_url = "https://www.newscientist.com/subject/technology/"
     articles2 = scrape_main(news2_url)
-    for arti in articles:
+   
+    for arti in articles2:
         if arti is None: continue
         ar = article.objects.create(
             title = arti['title'],
@@ -22,7 +31,7 @@ def load_news(request):
             link = arti['link']
         )
         ar.save()
-    for arti in articles2:
+    for arti in articles:
         if arti is None: continue
         ar = article.objects.create(
             title = arti['title'],
